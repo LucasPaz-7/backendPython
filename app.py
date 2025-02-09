c5f4bfd597ead511ece49282133c5e8c26513521
Lucas Paz <lucaspaz283@gmail.com>
2025-02-09 17:08:35 -0300

    CORS

--- app.py
+++ app.py
@@ -9,10 +9,12 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required, ge
 from werkzeug.security import generate_password_hash, check_password_hash
 from flasgger import Swagger, swag_from
 import logging
+from flask_cors import CORS
 
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'super-secret-key')
